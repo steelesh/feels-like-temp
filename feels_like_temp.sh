228bd0b6ca42872d93f4e6e37464245b9f3d896c
Steele Shreve <94927484+steelesh@users.noreply.github.com>
2024-10-23 13:56:48 -0500

    remove color (added complexity)

--- feels_like_temp.sh
+++ feels_like_temp.sh
@@ -7,26 +7,19 @@ country_code=""
 log_file=""
 api_key="" # insert your api key here
 
-# color codes
-red_text="\033[31m"
-green_text="\033[32m"
-yellow_text="\033[33m"
-blue_text="\033[34m"
-reset="\033[0m"
-
 
 
 # display help and usage instructions
 function help {
     echo
-    echo "${green_text}usage:${reset} $0 [-c city_name] [-s state_code] [-C country_code] [-l log_file] [-h]"
+    echo "usage: $0 [-c city_name] [-s state_code] [-C country_code] [-l log_file] [-h]"
     echo
-    echo "${blue_text}options:${reset}"
-    echo "    ${yellow_text}-c${reset}   specify the ${blue_text}city name${reset} (regex: only alphabetic characters and spaces)"
-    echo "    ${yellow_text}-s${reset}   specify the ${blue_text}state code${reset} (optional, regex: 2 alphabetic characters)"
-    echo "    ${yellow_text}-C${reset}   specify the ${blue_text}country code${reset} (optional, regex: 2 alphabetic characters, defaults to \"us\" if state code is provided)"
-    echo "    ${yellow_text}-l${reset}   log the output to a file"
-    echo "    ${yellow_text}-h${reset}   display this ${red_text}help${reset} message"
+    echo "options:"
+    echo "    -c$   specify the city name (regex: only alphabetic characters and spaces)"
+    echo "    -s$   specify the state code (optional, regex: 2 alphabetic characters)"
+    echo "    -C$   specify the country code (optional, regex: 2 alphabetic characters, defaults to \"us\" if state code is provided)"
+    echo "    -l$   log the output to a file"
+    echo "    -h$   display this help message"
     echo
     exit 1
 }
@@ -50,25 +43,25 @@ done
 # early exit if required input is null OR input does not match regex
 if [[ -z "$city_name" ]]; then
     echo
-    echo "${red_text}error: city name is required${reset}"
+    echo "error: city name is required"
     echo
     help
 fi
 if [[ ! "$city_name" =~ ^[a-zA-Z\ ]+$ ]]; then
     echo
-    echo "${red_text}error: invalid city name, it should contain only alphabetic characters and spaces${reset}"
+    echo "error: invalid city name, it should contain only alphabetic characters and spaces"
     echo
     exit 1
 fi
 if [[ -n "$state_code" ]] && [[ ! "$state_code" =~ ^[a-zA-Z]{2}$ ]]; then
     echo
-    echo "${red_text}error: invalid state code, it should be exactly 2 alphabetic characters${reset}"
+    echo "error: invalid state code, it should be exactly 2 alphabetic characters"
     echo
     exit 1
 fi
 if [[ -n "$country_code" ]] && [[ ! "$country_code" =~ ^[a-zA-Z]{2}$ ]]; then
     echo
-    echo "${red_text}error: invalid country code, it should be exactly 2 alphabetic characters${reset}"
+    echo "error: invalid country code, it should be exactly 2 alphabetic characters"
     echo
     exit 1
 fi
@@ -101,7 +94,7 @@ geo_res=$(curl -s "http://api.openweathermap.org/geo/1.0/direct?q=${q}&limit=5&a
 # exit if empty or invalid response
 if [ -z "$geo_res" ]; then
     echo
-    echo "${red_text}error: couldn't find location data for ${q}${reset}"
+    echo "error: couldn't find location data for ${q}"
     echo
     exit 1
 fi
@@ -113,9 +106,9 @@ location_count=$(echo "$geo_res" | jq '. | length')
 if [ "$location_count" -gt 1 ]; then
 
     echo
-    echo "multiple locations found for ${yellow_text}'${city_name}'${reset}:"
+    echo "multiple locations found for '${city_name}':"
     echo
-    echo "${red_text}none of these?${reset} press ctrl+c to exit"
+    echo "none of these? press ctrl+c to exit"
     echo
     echo
 
@@ -126,7 +119,7 @@ if [ "$location_count" -gt 1 ]; then
         lat=$(echo "$geo_res" | jq -r ".[$i].lat")
         lon=$(echo "$geo_res" | jq -r ".[$i].lon")
 
-        echo "${blue_text}$((i+1)).${reset} ${city}, ${state}, ${country} (lat: $lat, lon: $lon)"
+        echo "$((i+1)). ${city}, ${state}, ${country} (lat: $lat, lon: $lon)"
     done
     
     echo
@@ -136,13 +129,13 @@ if [ "$location_count" -gt 1 ]; then
 
     if [[ ! "$location_choice" =~ ^[0-9]+$ ]]; then
         echo
-        echo "${red_text}error: invalid location choice, it should be a number${reset}"
+        echo "error: invalid location choice, it should be a number"
         echo
         exit 1
     fi
     if [[ "$location_choice" -lt 1 ]] || [[ "$location_choice" -gt "$location_count" ]]; then
         echo
-        echo "${red_text}error: invalid location choice, it should be between 1 and ${location_count}${reset}"
+        echo "error: invalid location choice, it should be between 1 and ${location_count}"
         echo
         exit 1
     fi
@@ -155,7 +148,7 @@ if [ "$location_count" -gt 1 ]; then
 
     echo
     echo
-    echo "you selected location ${blue_text}${location_choice}:${reset} ${yellow_text}${city}, ${state}, ${country} (lat: $lat, lon: $lon)${reset}"
+    echo "you selected location ${location_choice}: ${city}, ${state}, ${country} (lat: $lat, lon: $lon)"
 
 elif [ "$location_count" -eq 1 ]; then
     lat=$(echo "$geo_res" | jq -r '.[0].lat')
@@ -166,11 +159,11 @@ elif [ "$location_count" -eq 1 ]; then
 
     echo
     echo
-    echo "location found: ${yellow_text}${city}, ${state}, ${country} (lat: $lat, lon: $lon)${reset}"
+    echo "location found: ${city}, ${state}, ${country} (lat: $lat, lon: $lon)"
 
 else
     echo
-    echo "${red_text}error: couldn't find any location data for '${q}'${reset}"
+    echo "error: couldn't find any location data for '${q}'"
     echo
     exit 1
 fi
@@ -180,7 +173,7 @@ fi
 # early exit if lat or lon is null
 if [ "$lat" == "null" ] || [ "$lon" == "null" ]; then
     echo
-    echo "${red_text}error: couldn't find latitude and/or longitude for ${q}${reset}"
+    echo "error: couldn't find latitude and/or longitude for ${q}"
     echo
     exit 1
 fi
@@ -209,7 +202,7 @@ fi
 # print the output to the command line
 echo
 echo
-echo "${green_text}output:${reset} $output"
+echo "output: $output"
 
 
 
@@ -217,9 +210,9 @@ echo "${green_text}output:${reset} $output"
 if [ -n "$log_file" ]; then
     current_time=$(date +"%B %-d, %Y at %-I:%M%p %Z")
     if ! touch "$log_file" &> /dev/null; then
-        echo -e "${red_text}error: can't write to file ${log_file}${reset}"
+        echo -e "error: can't write to file ${log_file}"
         exit 1
     fi
     echo "[$current_time] $output" >> "$log_file"
-    echo "${green_text}output written to ${log_file}${reset}"
+    echo "output written to ${log_file}"
 fi
